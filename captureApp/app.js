baacdb5976c0ea7403bc6a65bca1e21dddfcbe6e
Rohan Verma <rohanv@AtosITv-DLuce.northamerica.corp.microsoft.com>
2019-07-20 21:12:27 +0530

    add save function

--- captureApp/app.js
+++ captureApp/app.js
@@ -1,33 +1,49 @@
-var constraints = {
-    video: {
-        facingMode: "environment"
-    },
-    audio: false
-}
+// Set constraints for the video stream
+var constraints = { video: { facingMode: "user" }, audio: false };
+var track = null;
 
-const cameraView = document.querySelector('#camera--view'),
-    cameraOutput = document.querySelector('#camera--output'),
-    cameraSensor = document.querySelector('#camera--sensor'),
-    cameraTrigger = document.querySelector('#camera--trigger');
+// Define constants
+const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger");
 
+// Access the device camera and stream to cameraView
 function cameraStart() {
     navigator.mediaDevices
-    .getUserMedia(constraints)
-    .then(function(stream) {
-        track = stream.getTracks()[0];
-        cameraView.srcObject = stream;
-    })
-    .catch(function(error) {
-        console.error('Oop. something went wrong!', error);
-    });
+        .getUserMedia(constraints)
+        .then(function(stream) {
+            track = stream.getTracks()[0];
+            cameraView.srcObject = stream;
+        })
+        .catch(function(error) {
+            console.error("Oops. Something is broken.", error);
+        });
 }
 
-cameraTrigger.onClick = function() {
-    cameraSensor.width = cameraview.videoWidth;
-    cameraSensor.height = cameraView.videoWidth;
-    cameraSensor.getContext('2d').drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL('image/webp');
-    cameraOutput.classList.add('taken');
+// Take a picture when cameraTrigger is tapped
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+    // track.stop();
+};
+
+// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
+
+
+// Install ServiceWorker
+if ('serviceWorker' in navigator) {
+  console.log('CLIENT: service worker registration in progress.');
+  navigator.serviceWorker.register( '/camera-app/part-2/sw.js' , { scope : ' ' } ).then(function() {
+    console.log('CLIENT: service worker registration complete.');
+  }, function() {
+    console.log('CLIENT: service worker registration failure.');
+  });
+} else {
+  console.log('CLIENT: service worker is not supported.');
 }
 
-window.addEventListener('load', cameraStart, false);
